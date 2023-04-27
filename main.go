d8df9ca5f2b197a256af164ef7a5b45c892c9805
RianIhsan <rianwhatsup@gmail.com>
2023-04-27 22:05:48 +0700

    add cors

--- main.go
+++ main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"github.com/RianIhsan/go-restapi-gin/controllers/productcontroller"
 	"github.com/RianIhsan/go-restapi-gin/models"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	App := gin.Default()
 	models.ConnectDatabase()
+
+	config := cors.DefaultConfig()
+	config.AllowOrigins = []string{"*"}
+	App.Use(cors.New(config))
 
 	App.GET("/api/products/", productcontroller.Index)
 	App.GET("/api/products/:id", productcontroller.Read)
